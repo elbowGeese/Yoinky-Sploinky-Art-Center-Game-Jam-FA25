57da1c2f1660d454207c1048efe46b3a1cd7503c
Pompoko-Barry <bluerocks25426@gmail.com>
2025-10-10 17:39:06 -0700

    Update MousePosition.cs

--- ArtCenterGameJamFA25/Assets/Scripts/MousePosition.cs
+++ ArtCenterGameJamFA25/Assets/Scripts/MousePosition.cs
@@ -7,18 +7,31 @@ public class MousePosition : MonoBehaviour
 {
     public Vector2 screenPosition;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public Vector3 worldPosition;
+
 
     // Update is called once per frame
     void Update()
     {
        //screenPosition = Input.mousePosition;  //this is old input system
-      screenPosition =  Mouse.current.position.ReadValue();
-      
+     screenPosition =  Mouse.current.position.ReadValue();
 
+       Vector3 screenPositionWithZ = new Vector3(screenPosition.x, screenPosition.y, Camera.main.nearClipPlane + 1f);
+
+        worldPosition = Camera.main.ScreenToWorldPoint(screenPositionWithZ);
+        
+
+        transform.position = worldPosition;
+
+    }
+
+    public void OnMouseDown()
+    {
+
+    }
+
+    public void OnMouseUp()
+    {
+        
     }
 }
