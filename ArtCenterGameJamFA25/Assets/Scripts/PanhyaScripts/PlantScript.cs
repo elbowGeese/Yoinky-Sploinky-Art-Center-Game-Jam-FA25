b945296c7051f9c0b41e42f5c47e9735b74db752
katalover420 <ownedbyanowl@gmail.com>
2025-10-10 18:42:19 -0700

    plant scripts

--- ArtCenterGameJamFA25/Assets/Scripts/PanhyaScripts/PlantScript.cs
+++ ArtCenterGameJamFA25/Assets/Scripts/PanhyaScripts/PlantScript.cs
@@ -2,8 +2,15 @@ using UnityEngine;
 
 public class PlantScript : MonoBehaviour
 {
-
+    public SunlightStates SunlightScript;
     public int plantGrowth;
+    public bool wantsMaxSun;
+    public bool wantsSun;
+    public bool wantsMinSun;
+    public bool wantsNoSun;
+    public float time;
+
+    public bool burning;
     public enum ObjectState
     {
         MaxGrowth,
@@ -22,41 +29,172 @@ public class PlantScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        switch (currentState)
+        time += Time.deltaTime;
+        if (time >=1)
         {
-            case ObjectState.MaxGrowth:
-                mGrowth();
-                break;
-            case ObjectState.Growth:
-                Grow();
-                break;
-            case ObjectState.Decay:
-                DecayFunction();
-                break;
-            case ObjectState.MaxDecay:
-                MaxDecayFunction();
-                break;
-
+            mGrowth();
+            
+            Grow();
+            DecayFunction();
+            MaxDecayFunction();
+            time = 0;
         }
+        
+        //switch (currentState)
+        //{
+        //    case ObjectState.MaxGrowth:
+        //        mGrowth();
+        //        break;
+        //    case ObjectState.Growth:
+        //        Grow();
+        //        break;
+        //    case ObjectState.Decay:
+        //        DecayFunction();
+        //        break;
+        //    case ObjectState.MaxDecay:
+        //        MaxDecayFunction();
+        //        break;
+
+        //}
     }
 
     void mGrowth()
     {
-        
+        if (wantsMaxSun == true)
+        {
+
+            if (SunlightScript.maxsun == true)
+            {
+
+                plantGrowth += 2;
+
+            }
+            if (wantsSun == true)
+            {
+                if (SunlightScript.normalsun == true)
+                {
+                    plantGrowth += 2;
+                }
+
+            }
+            if (wantsMinSun == true)
+            {
+                if (SunlightScript.minsun == true)
+                {
+                    plantGrowth += 2;
+                }
+
+            }
+            if (wantsNoSun == true)
+            {
+                if (SunlightScript.nosun == true)
+                {
+                    plantGrowth += 2;
+                }
+
+            }
+        }
     }
 
     void Grow()
     {
+        if (wantsMaxSun == true)
+        {
+            if (SunlightScript.normalsun == true)
+            {
+                plantGrowth += 1;
+            }
 
+        }
+        if (wantsSun == true)
+        {
+            if (SunlightScript.minsun == true)
+            {
+                plantGrowth += 1;
+            }
+
+        }
+        if (wantsMinSun == true)
+        {
+            if (SunlightScript.nosun == true)
+            {
+                plantGrowth +=1 ;
+            }
+
+        }
+        
     }
 
     void DecayFunction()
     {
+        if (wantsMaxSun == true)
+        {
+            if (SunlightScript.minsun == true)
+            {
+                plantGrowth -= 1;
+            }
 
+        }
+        if (wantsSun == true)
+        {
+            if (SunlightScript.nosun == true)
+            {
+                plantGrowth-= 1;
+            }
+
+        }
+        if (wantsMinSun == true)
+        {
+            if (SunlightScript.normalsun == true)
+            {
+                plantGrowth-= 1;
+            }
+
+        }
+        if (wantsNoSun == true)
+        {
+            if (SunlightScript.normalsun == true)
+            {
+                plantGrowth-= 1;
+            }
+
+        }
     }
 
     void MaxDecayFunction()
     {
+        if (wantsMaxSun == true)
+        {
+            if (SunlightScript.nosun == true)
+            {
+                plantGrowth =- 2;
+            }
 
+        }
+        if (wantsSun == true)
+        {
+            if (SunlightScript.nosun == true)
+            {
+                plantGrowth =- 2;
+            }
+
+        }
+        if (wantsMinSun == true)
+        {
+            if (SunlightScript.maxsun == true)
+            {
+                plantGrowth =- 2;
+            }
+
+        }
+        if (wantsNoSun == true)
+        {
+            if (SunlightScript.maxsun == true)
+            {
+                plantGrowth -=       2;
+            }
+
+        }
     }
 }
+
